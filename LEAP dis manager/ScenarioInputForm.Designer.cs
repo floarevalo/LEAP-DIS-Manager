23c429a0acb3501177a26a8d0c60ada7c4bf4cd8
mohamedmetwally123 <mohameda.metwally63@gmail.com>
2025-06-23 14:20:35 -0400

    add a new scenario

---
+++ LEAP dis manager/ScenarioInputForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LEAP_dis_manager
+{
+    partial class ScenarioInputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            newScenarioNameTextBox = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // newScenarioNameTextBox
+            // 
+            newScenarioNameTextBox.Location = new Point(165, 112);
+            newScenarioNameTextBox.Name = "newScenarioNameTextBox";
+            newScenarioNameTextBox.Size = new Size(188, 23);
+            newScenarioNameTextBox.TabIndex = 0;
+            newScenarioNameTextBox.Click += scenarioNameTextBox_Click;
+            newScenarioNameTextBox.TextChanged += scenarioNameTextBox_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(165, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Enter a New Senario Name:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(548, 220);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "OK";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += OK_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(467, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Cancel_Click;
+            // 
+            // ScenarioInputForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(639, 260);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(newScenarioNameTextBox);
+            Name = "ScenarioInputForm";
+            Text = "ScenarioInputForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox newScenarioNameTextBox;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ LEAP dis manager/ScenarioInputForm.cs
@@ -0,0 +1,129 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEAP_dis_manager
+{
+    public partial class ScenarioInputForm : Form
+    {
+        private string databaseIpAddress;
+        private int databasePort;
+        private string sectionId;
+        public ScenarioInputForm()
+        {
+            InitializeComponent();
+            loadDataBase();
+        }
+        private void loadDataBase()
+        {
+            databaseIpAddress = Properties.Settings.Default.databaseIpAddress;
+            databasePort = Properties.Settings.Default.databasePort;
+
+        }
+
+        public static void addSectionId(string sectionId, string dbIp, int dbPort)
+        {
+
+            string userId = "postgres";
+            string password = "postgres";
+            string databaseName = "LEAP";
+            string connectionString = $"Host={dbIp};Port={dbPort};Database={databaseName};User Id={userId};Password={password};";
+            try
+            {
+                using NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+                conn.Open();
+
+                //Check the database to see if a unit with the given Entity ID already exists
+                //this assumes that we use appplication_id and site_id to distinguish between different entities
+                string query = "SELECT COUNT(*) FROM sections WHERE sectionid = @section_id";
+                using NpgsqlCommand checkCommand = new NpgsqlCommand(query, conn);
+
+
+                //sectionID is a global variable
+                checkCommand.Parameters.AddWithValue("@section_id", sectionId);
+
+
+                int count = Convert.ToInt32(checkCommand.ExecuteScalar());
+
+                //That means that sectionid already exists
+                if (count > 0)
+                {
+                    //If a unit with the given Entity ID does not exist in the database, add it
+                    MessageBox.Show("Section ID already exists");
+ 
+                }
+                //sectionid doesn't exist so add it
+                else
+                {
+                    //TODO: add the online thing
+                    string insertQuery = "INSERT INTO sections (sectionid, isonline) VALUES (@section_id, @is_online)";
+                    using NpgsqlCommand insertCommand = new NpgsqlCommand(insertQuery, conn);
+                    insertCommand.Parameters.AddWithValue("@section_id", sectionId);
+                    insertCommand.Parameters.AddWithValue("@is_online", false);
+                    int rowsAffected = insertCommand.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Section ID has been inserted");
+
+                    }
+
+                }
+            }
+            catch (NpgsqlException npgsqlEx)
+            {
+                MessageBox.Show($"PostgreSQL Error: {npgsqlEx.Message}");
+                Console.WriteLine(npgsqlEx.ToString());
+            }
+            catch (SocketException socketEx)
+            {
+                MessageBox.Show($"Socket Error: {socketEx.Message}");
+                Console.WriteLine(socketEx.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"General Error: {ex.Message}");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+
+
+
+
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            addSectionId(sectionId, databaseIpAddress, databasePort);
+        }
+
+
+        private void scenarioNameTextBox_Click(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        private void scenarioNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            sectionId = newScenarioNameTextBox.Text;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
